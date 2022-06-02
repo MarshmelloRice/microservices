be2ab3d258e456c9c93a0644ca722400091dcf6f
marshmelloRice <xinxin99661@gmail.com>
2022-06-02 16:53:48 -0400

    deleted messages

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,67 +1,67 @@
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { OrderStatus } from '@k8s-demo/common';
-
-import { Order } from '../../models/order'; 
-import { app } from '../../app';
-import { stripe } from '../../stripe';
-import { Payment } from '../../models/payment';
-
-// jest.mock('../../stripe');
-
-it('returns a 404 when purchasing an order that does not exist',async () => {
-    await request(app)
-        .post('/api/payments')
-        .set('Cookie', global.signin())
-        .send({
-            token: 'qweqwe',
-            orderId: new mongoose.Types.ObjectId().toHexString(),
-        })
-        .expect(404);
-});
-
-it('returns a 401 when purchasing an order that does not belong to the user', async () => {
-    const order = Order.build({
-        id: new mongoose.Types.ObjectId().toHexString(),
-        userId: new mongoose.Types.ObjectId().toHexString(),
-        version: 0,
-        price: 20,
-        status: OrderStatus.Created
-    });
-    await order.save();
-
-    await request(app)
-        .post('/api/payments')
-        .set('Cookie', global.signin())
-        .send({
-            token: 'qweqwe',
-            orderId: order.id,
-        })
-        .expect(401);
-});
-
-it('returns a 400 when purchasing a cancelled order', async () => {
-    const userId = new mongoose.Types.ObjectId().toHexString();
-
-    const order = Order.build({
-        id: new mongoose.Types.ObjectId().toHexString(),
-        userId: userId,
-        version: 0,
-        price: 20,
-        status: OrderStatus.Cancelled
-    });
-    await order.save();
-
-    await request(app)
-        .post('/api/payments')
-        .set('Cookie', global.signin(userId))
-        .send({
-            orderId: order.id,
-            token: 'qweqwe'
-        })
-        .expect(400);
-});
-
+// import mongoose from 'mongoose';
+// import request from 'supertest';
+// import { OrderStatus } from '@k8s-demo/common';
+// 
+// import { Order } from '../../models/order'; 
+// import { app } from '../../app';
+// import { stripe } from '../../stripe';
+// import { Payment } from '../../models/payment';
+// 
+jest.mock('../../stripe');
+// 
+// it('returns a 404 when purchasing an order that does not exist',async () => {
+    // await request(app)
+        // .post('/api/payments')
+        // .set('Cookie', global.signin())
+        // .send({
+            // token: 'qweqwe',
+            // orderId: new mongoose.Types.ObjectId().toHexString(),
+        // })
+        // .expect(404);
+// });
+// 
+// it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+    // const order = Order.build({
+        // id: new mongoose.Types.ObjectId().toHexString(),
+        // userId: new mongoose.Types.ObjectId().toHexString(),
+        // version: 0,
+        // price: 20,
+        // status: OrderStatus.Created
+    // });
+    // await order.save();
+// 
+    // await request(app)
+        // .post('/api/payments')
+        // .set('Cookie', global.signin())
+        // .send({
+            // token: 'qweqwe',
+            // orderId: order.id,
+        // })
+        // .expect(401);
+// });
+// 
+// it('returns a 400 when purchasing a cancelled order', async () => {
+    // const userId = new mongoose.Types.ObjectId().toHexString();
+// 
+    // const order = Order.build({
+        // id: new mongoose.Types.ObjectId().toHexString(),
+        // userId: userId,
+        // version: 0,
+        // price: 20,
+        // status: OrderStatus.Cancelled
+    // });
+    // await order.save();
+// 
+    // await request(app)
+        // .post('/api/payments')
+        // .set('Cookie', global.signin(userId))
+        // .send({
+            // orderId: order.id,
+            // token: 'qweqwe'
+        // })
+        // .expect(400);
+// });
+// 
 // it('returns a 204 with vaild inputs', async () => {
     // const userId = new mongoose.Types.ObjectId().toHexString();
     // const price = Math.floor(Math.random() * 100000);
@@ -84,10 +84,10 @@ it('returns a 400 when purchasing a cancelled order', async () => {
         // })
         // .expect(201);
 // 
-    // const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
-    // expect(chargeOptions.source).toEqual('tok_visa');
-    // expect(chargeOptions.amount).toEqual(20 * 100);
-    // expect(chargeOptions.currency).toEqual('usd');
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(20 * 100);
+    expect(chargeOptions.currency).toEqual('usd');
 // 
     // const stripeCharges = await stripe.charges.list({ limit: 50});
     // const stripeCharge = stripeCharges.data.find(charge => {
