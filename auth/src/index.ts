5c7a3d9034cb06128e7e890242c333ec09e017a4
marshmelloRice <xinxin99661@gmail.com>
2022-06-02 15:55:54 -0400

    added messages

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,6 @@ import { app } from './app';
 
 const start = async () => {
     console.log("starting up...");
-    console.log("Hello World...");
-
 
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
